e4bcdbfea9c2efa4818811181b59c0b8e7f137ad
nj <nickgentrymma@gmail.com>
2023-06-14 09:52:27 -0700

    Finished setting up User Controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,19 @@
 class Api::UsersController < ApplicationController
+  wrap_parameters include: User.attribute_names + ['password']
+  
   def create
+    @user = User.new(user_params)
+    if @user.save 
+      login!(@user)
+      render json: @user 
+    else 
+      render json: { errors: @user.errors.full_messages }, statues: unprocessable_entity
+    end
+  end
+
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
