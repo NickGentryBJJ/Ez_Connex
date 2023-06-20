a1bf707a2ec6e5ee954a1506abc3460526e7ce2d
nj <nickgentrymma@gmail.com>
2023-06-19 21:56:36 -0700

    Started Production

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem "rails", "~> 7.0.5"
 
 gem "pg", "~> 1.1"
 
+  gem "faker"
 
 # Use the Puma web server [https://github.com/puma/puma]
 gem "puma", "~> 5.0"
@@ -40,7 +41,6 @@ gem "bootsnap", require: false
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "byebug", platforms: %i[ mri mingw x64_mingw ]
-  gem "faker"
 end
 
 group :development do
--- frontend/src/components/LoginFormPage/index.js
+++ frontend/src/components/LoginFormPage/index.js
@@ -56,59 +56,52 @@ function LoginFormPage() {
   
 
   return (
-    <div className='form-wrapper'>
-    
-    <h1 className='login-text'>Welcome to your <br/> professional community</h1>
-
-    <form onSubmit={handleSubmit}>
-      <ul>
-        {errors.map(error => <li className='errors' key={error}>{error}</li>)}
-      </ul>
-      <label className='email'>
-        Email <br />    
-        <div className='input-email'>
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            />
-          </div>
-      </label>
-      <br/>
-      <label className='password'>
-        Password <br />
-        <div className='input-password'>
-
-        <input
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-          />
-          </div>
-      </label>
-      <br/>
-      <button type="submit">Sign In</button>
-    </form>
-    <p>-------------- or ----------------</p>
-  
-    <NavLink to="/signup"> <button>
-      New to EZ Connex? Sign Up Now!
-      </button>
-      </NavLink>
+    <div className="page-wrapper">
+      <h1 className='login-text'>Welcome to your <br/> professional community</h1>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <ul>
+            {errors.map(error => <li className='errors' key={error}>{error}</li>)}
+          </ul>
+          <label className="email">
+            Email <br />    
+            <div className='input-email'>
+              <input
+                type="text"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                />
+            </div>
+          </label>
+          <label className='password'>
+            Password <br />
+            <div className='input-password'>
+              <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              />
+            </div>
+          </label>
+          <br/>
+          <button className='sign-in-button' type="submit">Sign In</button>
+        </form>
+        <p>-------------- or ----------------</p>
+        <NavLink to="/signup"><button className='sign-up-button'>
+          New to EZ Connex? Sign Up Now!
+          </button>
+        </NavLink>
         <br/>
           <button
           className='demo-button'
           onClick={demoUser}
-            type='submit'
+          type='submit'
           >
             Demo Log In
-            </button>
-      <div className='img-container'></div>
-
+          </button>
+        <div className='img-container'></div>
     </div>
-  );
+    );
 }
-
 export default LoginFormPage;
