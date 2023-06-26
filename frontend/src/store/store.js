b0d34b2a773b789436a812b1f23e584874b1cef7
nj <nickgentrymma@gmail.com>
2023-06-26 16:39:54 -0700

    added commentReducer to root reducer.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,11 +2,13 @@ import { createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import session from './session';
 import postReducer from './posts'
+import commentsReducer from './comments';
 
 
 const rootReducer = combineReducers({ 
     session, 
-    posts: postReducer 
+    posts: postReducer,
+    comments: commentsReducer
 });
 let enhancer;
 
