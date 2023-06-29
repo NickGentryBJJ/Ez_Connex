0cb4db09b817bf548042ad2be8d502caec783761
nj <nickgentrymma@gmail.com>
2023-06-29 14:12:54 -0700

    updated user model to has_many comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,6 +22,8 @@ class User < ApplicationRecord
 
   has_many :posts,
   dependent: :destroy
+  has_many :comments,
+  dependent: :destroy
   has_one_attached :photo
 
   def self.find_by_credentials(email, password)
