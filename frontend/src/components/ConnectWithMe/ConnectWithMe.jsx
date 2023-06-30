a6f63637dfd295f5fbd1e8c7112308f7606f1208
nj <nickgentrymma@gmail.com>
2023-06-29 22:02:20 -0700

    fixed git/linked links

--- frontend/src/components/ConnectWithMe/ConnectWithMe.jsx
+++ frontend/src/components/ConnectWithMe/ConnectWithMe.jsx
@@ -5,8 +5,8 @@ export default function ConnectWithMe() {
     return (
         <div className="connect-with-me-wrapper">
             <h1 className='c-w-m-header'>Connect With Me!</h1>
-            <a className='git-link' href="https://github.com/NickGentryBJJ"><BsGithub/></a>
-            <a className='linked-in-link' href="https://www.linkedin.com/in/nicholas-gentry-2721451b2/"><BsLinkedin/></a>
+           <a target="_blank" className='git-link' href="https://github.com/NickGentryBJJ"><BsGithub/></a>
+            <a target="_blank" className='linked-in-link' href="https://www.linkedin.com/in/nicholas-gentry-2721451b2/"><BsLinkedin/></a>
         </div>
     )
 }
